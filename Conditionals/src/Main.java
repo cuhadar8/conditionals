51a6baaeec94b55ffa3f32069652cee362bab14b
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-10-05 19:46:30 +0300

    conditionals

---
+++ Conditionals/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int sayi = 19;
+		if(sayi < 20) {
+			System.out.println("Sayı 20 den küçüktür.");
+		}
+		
+		int sayi2 = 14;
+		if(sayi2 < 20) {
+			System.out.println("Sayı 20 den küçüktür.");
+		}
+		
+		if(sayi2 < 15) {
+			System.out.println("Sayı 20 den küçüktür.");
+		}
+		
+		
+		int sayi3 = 24;
+		
+		if(sayi3 < 20) {
+			System.out.println("Sayı 20 den küçüktür.");
+		}
+		
+		else if(sayi3 == 20) {
+			System.out.println("Sayı 20 ye eşittir.");
+		}
+		
+		else {
+			System.out.println("Sayı 20 den büyüktür.");
+		}
+		
+	}
+
+}
